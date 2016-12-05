ad6791b4b4c3ab0579c70f8b177046c54233478b
Lefteris Zafiris <zaf@fastmail.com>
2016-12-05 16:08:41 +0000

    Updated cli script to use the Bing API. Small fixes and typos

--- cli/mstts-cli.pl
+++ cli/mstts-cli.pl
@@ -1,83 +1,70 @@
 #!/usr/bin/env perl
 
 #
-# Script that uses Microsoft Translator for text to speech synthesis.
+# Script that uses Bing Text To Speech API for speech synthesis.
 #
-# Copyright (C) 2012 - 2014, Lefteris Zafiris <zaf@fastmail.com>
+# Copyright (C) 2016, Lefteris Zafiris <zaf@fastmail.com>
 #
 # This program is free software, distributed under the terms of
 # the GNU General Public License Version 2.
-#
-# In order to use this script you have to subscribe to the Microsoft
-# Translator API on Azure Marketplace and register your application:
-# https://datamarket.azure.com/developer/applications/
 #
 
 
 use warnings;
 use strict;
+use utf8;
 use Encode qw(decode encode);
 use Getopt::Std;
 use File::Temp qw(tempfile);
-use URI::Escape;
 use LWP::UserAgent;
 use LWP::ConnCache;
 
-# --------------------------------------- #
-# Here you can assing your client ID and  #
-# client secret from Azure Marketplace.   #
-my $clientid     = "";
-my $clientsecret = "";
-# --------------------------------------- #
+# -------------------------------------- #
+# Here you can assign your Bing API Key  #
+my $key = "";
+# -------------------------------------- #
 
 my %options;
-my $input;
 my @text;
-my $tmpfh;
-my $tmpname;
-my @mp3list;
+my @rawlist;
 my $samplerate;
-my @soxargs;
-my $atoken;
-my $lang    = "en";
-my $format  = "audio/mp3";
-my $quality = "MaxQuality";
-my $level   = -3;
-my $speed   = 1;
-my $tmpdir  = "/tmp";
-my $timeout = 15;
-my $url     = "https://api.microsofttranslator.com/V2/Http.svc";
-my $mpg123  = `/usr/bin/which mpg123`;
-my $sox     = `/usr/bin/which sox`;
+my $input;
+my $lang      = "en-US";
+my $gender    = "Female";
+my $format    = "raw-16khz-16bit-mono-pcm";
+my $level     = -3;
+my $speed     = 1;
+my $tmpdir    = "/tmp";
+my $timeout   = 15;
+my $url       = "https://speech.platform.bing.com/synthesize";
+my $sox       = `/usr/bin/which sox`;
+my %lang_list = get_lang_list();
 
 VERSION_MESSAGE() if (!@ARGV);
 
-getopts('o:l:t:r:f:n:s:c:hqv', \%options);
+getopts('o:l:g:t:r:f:n:s:k:hqv', \%options);
 
-# Dislpay help messages #
+# Display help messages #
 VERSION_MESSAGE() if (defined $options{h});
 
-if (!$mpg123 || !$sox) {
-	say_msg("mpg123 or sox is missing. Aborting.");
+if (!$sox) {
+	say_msg("sox is missing. Aborting.");
 	exit 1;
 }
-chomp($mpg123, $sox);
+chomp($sox);
 
-# Initialise User angent #
+# Initialize User agent #
 my $ua = LWP::UserAgent->new(ssl_opts => {verify_hostname => 1});
 $ua->env_proxy;
 $ua->conn_cache(LWP::ConnCache->new());
 $ua->timeout($timeout);
 
-($clientid, $clientsecret) = split(/:/, $options{c}, 2) if (defined $options{c});
-$atoken = get_access_token();
-
-if (!$atoken) {
-	say_msg("You must have a client ID from Azure Marketplace to use this script.");
+parse_options();
+my $atoken = get_access_token();
+if (!exists $lang_list{"$lang-$gender"}) {
+	say_msg("Unsupported language/gender combination.");
 	exit 1;
 }
-
-parse_options();
 
 $input = decode('utf8', $input);
 for ($input) {
@@ -98,44 +85,55 @@ foreach my $line (@text) {
 	$line = encode('utf8', $line);
 	$line =~ s/^\s+|\s+$//g;
 	next if (length($line) == 0);
-	$line = uri_escape($line);
 
-	($tmpfh, $tmpname) = tempfile(
-		"mstts_XXXXXX",
-		SUFFIX => ".mp3",
+	my $data = "<speak version='1.0' xml:lang='" . $lang . "'><voice xml:lang='". $lang
+		. "' xml:gender='" . $gender . "' name='" . $lang_list{"$lang-$gender"}
+		. "'>" . $line ."</voice></speak>";
+	my $ua_response = $ua->post(
+		$url,
+		"Content-type" => "application/ssml+xml",
+		"X-Microsoft-OutputFormat" => $format,
+		"Authorization" => $atoken,
+		"Content" => $data,
+	);
+	if (!$ua_response->is_success) {
+		say_msg("Failed to fetch file:", $ua_response->status_line);
+		exit 1;
+	}
+
+	my ($tmpfh, $tmpname) = tempfile(
+		"bingtts_XXXXXX",
+		SUFFIX => ".raw",
 		DIR    => $tmpdir,
 		UNLINK => 1,
 	);
-
-	my $request = HTTP::Request->new('GET' =>
-		"$url/Speak?appid=$atoken&text=$line&language=$lang&format=$format&options=$quality"
-	);
-	my $response = $ua->request($request, $tmpname);
-	if (!$response->is_success) {
-		say_msg("Failed to fetch speech data!");
-		exit 1;
-	} else {
-		push(@mp3list, $tmpname);
-	}
+	binmode $tmpfh;
+	print $tmpfh $ua_response->decoded_content( charset => 'none' );
+	close $tmpfh;
+	push(@rawlist, ('-r', '16000', '-b', '16', '-c', '1', '-e', 'signed-integer', $tmpname));
 }
 
-# decode mp3s and concatenate #
+# concatenate sound files #
 my ($wavfh, $wavname) = tempfile(
-	"mstts_XXXXXX",
+	"bingtts_XXXXXX",
 	SUFFIX => ".wav",
 	DIR    => $tmpdir,
 	UNLINK => 1
 );
-if (system($mpg123, "-q", "-w", $wavname, @mp3list)) {
-	say_msg("mpg123 failed to process sound file.");
+if (system($sox, @rawlist, $wavname)) {
+	say_msg("sox failed to concatenate sound files.");
 	exit 1;
 }
 
 # Set sox args and process wav file #
-@soxargs = ($sox, "-q", "--norm=$level", $wavname);
-defined $options{o} ? push(@soxargs, ($options{o})) : push(@soxargs, ("-t", "alsa", "-d"));
-push(@soxargs, ("tempo", "-s", $speed)) if ($speed != 1);
-push(@soxargs, ("rate", $samplerate)) if ($samplerate);
+my @soxargs = ($sox, '-q', "--norm=$level", $wavname);
+if (defined $options{o}) {
+	push(@soxargs, ($options{o}));
+} else {
+	push(@soxargs, ('-t', 'alsa', '-d'));
+}
+push(@soxargs, ('tempo', '-s', $speed)) if ($speed != 1);
+push(@soxargs, ('rate', $samplerate)) if ($samplerate);
 
 if (system(@soxargs)) {
 	say_msg("sox failed to process sound file.");
@@ -146,26 +144,27 @@ exit 0;
 
 sub get_access_token {
 	# Obtaining an Access Token #
-	my $token;
-	my $response = $ua->post(
-		"https://datamarket.accesscontrol.windows.net/v2/OAuth2-13/",
-		[
-			client_id     => $clientid,
-			client_secret => $clientsecret,
-			scope         => 'http://api.microsofttranslator.com',
-			grant_type    => 'client_credentials',
-		],
-	);
-	if ($response->is_success and $response->content =~ /^\{"token_type":".+?","access_token":"(.+?)","expires_in":".+?","scope":".+?"\}$/) {
-		$token = uri_escape("Bearer $1");
-	} else {
-		say_msg("Failed to get Access Token.");
+	my $token = '';
+	if (!$key) {
+		say_msg("No API Key provided.");
+		exit 1;
 	}
+	my $response = $ua->post(
+		"https://api.cognitive.microsoft.com/sts/v1.0/issueToken",
+		"Ocp-Apim-Subscription-Key" => $key,
+	);
+	if ($response->is_success) {
+		$token = "Bearer " . $response->content;
+		} else {
+			say_msg("Failed to get Access Token:", $response->status_line);
+			exit 1;
+		}
 	return $token;
 }
 
 sub parse_options {
-	lang_list() if (defined $options{v});
+	print_lang_list() if (defined $options{v});
+	$key = $options{k} if (defined $options{k});
 	# Get input text #
 	if (defined $options{t}) {
 		$input = $options{t};
@@ -175,77 +174,116 @@ sub parse_options {
 			close($fh);
 		} else {
 			say_msg("Cant read file $options{f}");
-			exit 1;
 		}
-	} else {
-		say_msg("No text passed for synthesis.");
-		exit 1;
 	}
-	# check if language setting is valid #
+	# check if language and gender settings are valid #
 	if (defined $options{l}) {
-		$options{l} =~ /^[a-zA-Z]{2}(-[a-zA-Z]{2,3})?$/ ? $lang = $options{l}
-			: say_msg("Invalid language setting, using default.");
+		if ($options{l} =~ /^[a-z]{2}-[A-Z]{2}$/) {
+			$lang = $options{l};
+		} else {
+			say_msg("Invalid language setting, using default.");
+		}
+	}
+	if (defined $options{g}) {
+		if ($options{l} =~ /^(f|m)$/i) {
+			$gender = $options{g};
+		} else {
+			say_msg("Invalid gender setting, using default.");
+		}
 	}
 	# set audio sample rate #
 	if (defined $options{r}) {
-		$options{r} =~ /\d+/ ? $samplerate = $options{r}
-			: say_msg("Invalid sample rate, using default.");
+		if ($options{r} =~ /\d+/) {
+			$samplerate = $options{r};
+		} else {
+			say_msg("Invalid sample rate, using default.");
+		}
 	}
 	# set speed factor #
 	if (defined $options{s}) {
-		$options{s} =~ /\d+/ ? $speed = $options{s}
-			: say_msg("Invalind speed factor, using default.");
+		if ($options{s} =~ /\d+/) {
+			$speed = $options{s};
+		} else {
+			say_msg("Invalid speed factor, using default.");
+		}
 	}
-	# set the audio normalisation level #
+	# set the audio normalization level #
 	if (defined $options{n}) {
-		$options{n} =~ /\d+/ ? $level = $options{n}
-			: say_msg("Invalind normalisation level, using default.");
+		if ($options{n} =~ /\d+/) {
+			$level = $options{n};
+		} else {
+			say_msg("Invalid normalization level, using default.");
+		}
 	}
 	return;
 }
 
-sub lang_list {
-	# Display the list of supported languages #
-	my $request = HTTP::Request->new('GET' => "$url/GetLanguagesForSpeak?appid=$atoken");
-	my $response = $ua->request($request);
-	if ($response->is_success) {
-		print "Supported languages list:\n",
-			join("\n", grep(/[a-zA-Z\-]{2,}/, split(/<.+?>/, $response->content))), "\n";
-	} else {
-		say_msg("Failed to fetch language list.");
-	}
+sub print_lang_list {
+	print "Supported Language list:\n";
+	print "$_\n" for (keys %lang_list);
 	exit 1;
+}
+
+sub get_lang_list {
+	my %list = (
+		"ar-EG-Female"	=> "Microsoft Server Speech Text to Speech Voice (ar-EG, Hoda)",
+		"de-DE-Female"	=> "Microsoft Server Speech Text to Speech Voice (de-DE, Hedda)",
+		"de-DE-Male"	=> "Microsoft Server Speech Text to Speech Voice (de-DE, Stefan, Apollo)",
+		"en-AU-Female"	=> "Microsoft Server Speech Text to Speech Voice (en-AU, Catherine)",
+		"en-CA-Female"	=> "Microsoft Server Speech Text to Speech Voice (en-CA, Linda)",
+		"en-GB-Female"	=> "Microsoft Server Speech Text to Speech Voice (en-GB, Susan, Apollo)",
+		"en-GB-Male"	=> "Microsoft Server Speech Text to Speech Voice (en-GB, George, Apollo)",
+		"en-IN-Male"	=> "Microsoft Server Speech Text to Speech Voice (en-IN, Ravi, Apollo)",
+		"en-US-Female"	=> "Microsoft Server Speech Text to Speech Voice (en-US, ZiraRUS)",
+		"en-US-Male"	=> "Microsoft Server Speech Text to Speech Voice (en-US, BenjaminRUS)",
+		"es-ES-Female"	=> "Microsoft Server Speech Text to Speech Voice (es-ES, Laura, Apollo)",
+		"es-ES-Male"	=> "Microsoft Server Speech Text to Speech Voice (es-ES, Pablo, Apollo)",
+		"es-MX-Male"	=> "Microsoft Server Speech Text to Speech Voice (es-MX, Raul, Apollo)",
+		"fr-CA-Female"	=> "Microsoft Server Speech Text to Speech Voice (fr-CA, Caroline)",
+		"fr-FR-Female"	=> "Microsoft Server Speech Text to Speech Voice (fr-FR, Julie, Apollo)",
+		"fr-FR-Male"	=> "Microsoft Server Speech Text to Speech Voice (fr-FR, Paul, Apollo)",
+		"it-IT-Male"	=> "Microsoft Server Speech Text to Speech Voice (it-IT, Cosimo, Apollo)",
+		"ja-JP-Female"	=> "Microsoft Server Speech Text to Speech Voice (ja-JP, Ayumi, Apollo)",
+		"ja-JP-Male"	=> "Microsoft Server Speech Text to Speech Voice (ja-JP, Ichiro, Apollo)",
+		"pt-BR-Male"	=> "Microsoft Server Speech Text to Speech Voice (pt-BR, Daniel, Apollo)",
+		"ru-RU-Female"	=> "Microsoft Server Speech Text to Speech Voice (ru-RU, Irina, Apollo)",
+		"ru-RU-Male"	=> "Microsoft Server Speech Text to Speech Voice (ru-RU, Pavel, Apollo)",
+		"zh-CN-Female"	=> "Microsoft Server Speech Text to Speech Voice (zh-CN, HuihuiRUS)",
+		"zh-CN-Female"	=> "Microsoft Server Speech Text to Speech Voice (zh-CN, Yaoyao, Apollo)",
+		"zh-CN-Male"	=> "Microsoft Server Speech Text to Speech Voice (zh-CN, Kangkang, Apollo)",
+		"zh-HK-Female"	=> "Microsoft Server Speech Text to Speech Voice (zh-HK, Tracy, Apollo)",
+		"zh-HK-Male"	=> "Microsoft Server Speech Text to Speech Voice (zh-HK, Danny, Apollo)",
+		"zh-TW-Female"	=> "Microsoft Server Speech Text to Speech Voice (zh-TW, Yating, Apollo)",
+		"zh-TW-Male"	=> "Microsoft Server Speech Text to Speech Voice (zh-TW, Zhiwei, Apollo)",
+	);
+	return %list;
 }
 
 sub say_msg {
 	# Print messages to user if 'quiet' flag is not set #
-	my @message = @_;
-	warn @message if (!defined $options{q});
+	my $message = join(" ", @_);
+	warn $message if (!defined $options{q});
 	return;
 }
 
 sub VERSION_MESSAGE {
 	# Help message #
-	print "Text to speech synthesis using Microsoft Translator API.\n\n",
-		"In order to use this script you have to subscribe to the Microsoft\n",
-		"Translator API on Azure Marketplace:\n",
-		"https://datamarket.azure.com/developer/applications/\n",
-		"Existing API Keys from http://www.bing.com/developers/appids.aspx\n",
-		"still work but they are considered deprecated and this method is no longer supported.\n\n",
+	print "Text to speech synthesis using Bing TTS API.\n\n",
 		"Supported options:\n",
 		" -t <text>      text string to synthesize\n",
 		" -f <file>      text file to synthesize\n",
-		" -l <lang>      specify the language to use, defaults to 'en' (English)\n",
+		" -l <lang>      specify the language to use, defaults to 'en-US' (US English)\n",
+		" -g <gender>    specify the voice gender, defaults to 'f' (female)\n",
 		" -r <rate>      specify the output sampling rate in Hertz (default 16000)\n",
 		" -o <filename>  save output as file\n",
-		" -n <dB-level>  normalise the audio to the given level (default -3)\n",
+		" -n <dB-level>  normalize the audio to the given level (default -3)\n",
 		" -s <factor>    specify the speech rate speed factor (default 1.0)\n",
-		" -c <clientid>  set the Azure marketplace credentials (clientid:clientsecret)\n",
+		" -k <key>       set the Bing API key\n",
 		" -q             quiet (Don't print any messages or warnings)\n",
 		" -h             this help message\n",
-		" -v             suppoted languages list\n\n",
+		" -v             supported languages list\n\n",
 		"Examples:\n",
-		"$0 -l en -t \"Hello world\"\n\tHave the synthesized speech played back to the user.\n",
-		"$0 -o hello.wav -l en -t \"Hello world\"\n\tSave the synthesized speech as a sound file.\n\n";
+		"$0 -t \"Hello world\"\n\tHave the synthesized speech played back to the user.\n",
+		"$0 -o hello.wav -l en-GB -t \"Hello world\"\n\tSave the synthesized speech as a sound file.\n\n";
 	exit 1;
 }
